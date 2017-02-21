2f550594ff995806dbf5aa771e4f4a6adde9da59
Mohit chattlani <chattlanimohit@gmail.com>
2017-02-22 00:46:43 +0530

    Update tttjs.js
    Corrected the logic of the code .

--- tttjs.js
+++ tttjs.js
@@ -44,12 +44,12 @@ function func2(id){
                     var b8=document.getElementById('b8').value;
                     var b9=document.getElementById('b9').value;
 
-                    if((b1==x && b5==x && b9==x)||(b3==x && b5==x && b6==x)||(b1==x && b2==x && b3==x)||(b4==x && b5==x && b6==x)||(b7==x && b8==x && b9==x)||(b1==x && b4==x && b7==x)||(b2==x && b5==x && b8==x)||(b3==x && b6==x && b9==x)){
+                    if((b1==x && b5==x && b9==x)||(b3==x && b5==x && b7==x)||(b1==x && b2==x && b3==x)||(b4==x && b5==x && b6==x)||(b7==x && b8==x && b9==x)||(b1==x && b4==x && b7==x)||(b2==x && b5==x && b8==x)||(b3==x && b6==x && b9==x)){
                         window.alert("player1 WON ");
                     reset();
             
                     }
-                    if((b1==o && b5==o && b9==o)||(b3==o && b5==o && b6==o)||(b1==o && b2==o && b3==o)||(b4==o && b5==o && b6==o)||(b7==o && b8==o && b9==o)||(b1==o && b4==o && b7==o)||(b2==o && b5==o && b8==o)||(b3==o && b6==o && b9==o)){
+                    if((b1==o && b5==o && b9==o)||(b3==o && b5==o && b7==o)||(b1==o && b2==o && b3==o)||(b4==o && b5==o && b6==o)||(b7==o && b8==o && b9==o)||(b1==o && b4==o && b7==o)||(b2==o && b5==o && b8==o)||(b3==o && b6==o && b9==o)){
                         window.alert("player2 WON ");
                         reset();
         
@@ -74,14 +74,14 @@ function func2(id){
                     var b8=document.getElementById('b8').value;
                     var b9=document.getElementById('b9').value;
 
-                    if((b1==x && b5==x && b9==x)||(b3==x && b5==x && b6==x)||(b1==x && b2==x && b3==x)||(b4==x && b5==x && b6==x)||(b7==x && b8==x && b9==x)||(b1==x && b4==x && b7==x)||(b2==x && b5==x && b8==x)||(b3==x && b6==x && b9==x)){
+                    if((b1==x && b5==x && b9==x)||(b3==x && b5==x && b7==x)||(b1==x && b2==x && b3==x)||(b4==x && b5==x && b6==x)||(b7==x && b8==x && b9==x)||(b1==x && b4==x && b7==x)||(b2==x && b5==x && b8==x)||(b3==x && b6==x && b9==x)){
                         window.alert("player1 WON ");
                         reset();
                         
                     }
 
 
-                    if((b1==o && b5==o && b9==o)||(b3==o && b5==o && b6==o)||(b1==o && b2==o && b3==o)||(b4==o && b5==o && b6==o)||(b7==o && b8==o && b9==o)||(b1==o && b4==o && b7==o)||(b2==o && b5==o && b8==o)||(b3==o && b6==o && b9==o)){
+                    if((b1==o && b5==o && b9==o)||(b3==o && b5==o && b7==o)||(b1==o && b2==o && b3==o)||(b4==o && b5==o && b6==o)||(b7==o && b8==o && b9==o)||(b1==o && b4==o && b7==o)||(b2==o && b5==o && b8==o)||(b3==o && b6==o && b9==o)){
                         window.alert("player2 WON ");
                         reset();
                     
